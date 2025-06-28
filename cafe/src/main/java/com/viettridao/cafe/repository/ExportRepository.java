fb05e7945d71899d051e5ed4f5a25bfb1235df98
NguyenVanCao-98 <hoathinhhay98@gmail.com>
2025-06-28 18:36:00 +0700

    Xóa thư mục cafe khỏi project

--- cafe/src/main/java/com/viettridao/cafe/repository/ExportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.viettridao.cafe.repository;
-
-import com.viettridao.cafe.model.ExportEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExportRepository extends JpaRepository<ExportEntity, Integer> {
-}
--- cafe/src/main/java/com/viettridao/cafe/service/ExportService.java
+++
@@ -1,8 +0,0 @@
-package com.viettridao.cafe.service;
-
-import com.viettridao.cafe.dto.request.export.CreateExportRequest;
-import com.viettridao.cafe.model.ExportEntity;
-
-public interface ExportService {
-    ExportEntity createExport(CreateExportRequest request);
-}
